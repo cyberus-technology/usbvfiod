0070b29e7f2b68bc59e94f8e950b988d74bb1dd4
Lukas Beierlieb <lukas.beierlieb@cyberus-technology.de>
2025-07-09 11:33:09 +0200

    pass through control transfers

--- src/device/pci/xhci.rs
+++ src/device/pci/xhci.rs
@@ -271,7 +271,7 @@ impl XhciController {
         self.real_device
             .as_ref()
             .unwrap()
-            .control_transfer(&request);
+            .control_transfer(&request, &self.dma_bus);
 
         // send transfer event
         let trb =
