3949d91332771175c9454b1fbfe6a1f405e0087a
Noi0103 <noi0103@proton.me>
2026-07-10 15:19:14 +0200

    endpoint_handle: control request with multiple trb in data stage td
    This enables a Data Stage to be composed of more than a single
    DataStageTrb.

--- src/device/xhci/usbrequest.rs
+++ src/device/xhci/usbrequest.rs
@@ -2,16 +2,10 @@
 
 /// Represents a USB control request.
 ///
-/// For documentation of the fields other than `address`, see Section "9.3 USB
+/// See xhci specification chapter 6.4.1.2.
+///
+/// For additional documentation of the fields other than `address`, see Section "9.3 USB
 /// Device Requests" in the USB 2.0 specification.
-///
-/// A request without data is packaged in two TRBs (a Setup Stage and a
-/// Status Stage). `data` should then be `None`.
-///
-/// A request with data is packaged in three TRBs (a Setup Stage, a Data
-/// Stage and a Status Stage). `data` should then contain the pointer
-/// from the Data Stage).
-///
 #[derive(Debug, PartialEq, Eq, Clone, Default)]
 pub struct UsbRequest {
     /// The guest address of the Status Stage of this request.
@@ -20,24 +14,11 @@ pub struct UsbRequest {
     pub request: u8,
     pub value: u16,
     pub index: u16,
+    /// The Setup Stage TRB's wLength field is supposed to be used to communicate
+    /// the sum of all transfer_length fields following in the Data TD. It is not
+    /// guaranteed, so we cannot use it. We still use the wLength fields size
+    /// of 16 bit to assume a maximum length for any control request.
     pub length: u16,
     pub data_pointer: Option<u64>,
     pub data: Vec<u8>,
-}
-
-impl UsbRequest {
-    // so that the control endpoint handler can make a copy
-    // (to store the request between submit_trb and next_complete)
-    pub const fn clone_without_data(&self) -> Self {
-        Self {
-            address: self.address,
-            request_type: self.request_type,
-            request: self.request,
-            value: self.value,
-            index: self.index,
-            length: self.length,
-            data_pointer: self.data_pointer,
-            data: vec![],
-        }
-    }
 }
