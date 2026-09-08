2376820ae69028a5b1865396b03e58768f53ec79
Noi0103 <noi0103@proton.me>
2026-09-08 11:57:33 +0200

    devshell.nix: add profiling shell
    A new shell with utility for profiling usbvfiod and potentially other in
    this repo included binaries.
    
    Since kcachegrind is a GUI application, the extra shell is used instead
    of extending the default one.

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -33,6 +33,46 @@ let
     cargo build --bin remote
     ./target/debug/remote --socket ${hotplug-socket} --attach /dev/bus/usb/$1/$2
   '';
+
+  # Prepare cached binaries for other scripts and give some very likely needed output.
+  preparegrind = pkgs.writeShellScriptBin "preparegrind" ''
+    cargo build --bin usbvfiod
+    cargo build --bin remote
+    nix-build -A netboot.x86_64-linux '<nixpkgs/nixos/release.nix>'
+
+    echo ""
+    echo "FIND YOUR BUS/DEVICE NUMBERS"
+    lsusb
+    echo ""
+    echo "LIST OF COMMANDS TO USE IN THE GUEST ON THE BLOCKDEVICE:"
+    echo "sudo su"
+    echo "dd if=/dev/urandom of=/dev/sda count=100 bs=4M status=progress oflag=direct && poweroff"
+    echo "dd if=/dev/sda of=/dev/null count=100 bs=4M status=progress oflag=direct && poweroff"
+  '';
+
+  # Alternative to `runusbvfiod` that will generate cachegrind files.
+  cachegrind = pkgs.writeShellScriptBin "cachegrind" ''
+    cargo build --bin usbvfiod
+    rm -rf ${vfio-user-socket} ${hotplug-socket}
+    ${pkgs.valgrind}/bin/valgrind --tool=cachegrind \
+      --cachegrind-out-file=cachegrind.out.%p \
+      --trace-children=yes \
+      ./target/debug/usbvfiod --socket-path ${vfio-user-socket} --hotplug-socket-path ${hotplug-socket} --no-color
+  '';
+
+  # Alternative to `runusbvfiod` that will generate callgrind files.
+  callgrind = pkgs.writeShellScriptBin "callgrind" ''
+    cargo build --bin usbvfiod
+    rm -rf ${vfio-user-socket} ${hotplug-socket}
+    ${pkgs.valgrind}/bin/valgrind --tool=callgrind \
+      --callgrind-out-file=callgrind.out.%p \
+      --compress-strings=no \
+      --separate-threads=yes \
+      --dump-line=yes \
+      --collect-systime=yes \
+      --collect-bus=yes \
+      ./target/debug/usbvfiod --socket-path ${vfio-user-socket} --hotplug-socket-path ${hotplug-socket} --no-color
+  '';
 in
 {
   default = craneLib.devShell {
@@ -43,6 +83,39 @@ in
       runusbvfiod
       runchv
       attach
+    ];
+  };
+
+  profiling = craneLib.devShell {
+    # used as guidance: https://nnethercote.github.io/perf-book/profiling.html
+    shellHook = ''
+      ${shellHook}
+      echo "INFO: This devShell is meant to be used for profiling."
+      echo "-----"
+      echo "INFO: It is recommended to apply the following to .cargo/config.toml:"
+      cat << EOF
+      [build]
+      rustflags = [
+        "-C",
+        "force-frame-pointers=yes",
+        "-C",
+        "symbol-mangling-version=v0",
+      ]
+      EOF
+      echo "-----"
+    '';
+    packages = [
+      sshhost
+      sshguest
+      runusbvfiod
+      runchv
+      attach
+
+      pkgs.valgrind
+      pkgs.kdePackages.kcachegrind
+      preparegrind
+      cachegrind
+      callgrind
     ];
   };
 }
