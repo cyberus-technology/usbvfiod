9cb816254cd60d901b39b82ac5fa3fed22c21617
Noi0103 <noi0103@proton.me>
2026-09-08 11:59:33 +0200

    devshell.nix: add more utils to the default shell
    QoL improvements to run manual tests with less typing.
    
    `runchv` is following the developer docs that describe a developer setup
    for cloud-hypervisor.

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -4,12 +4,34 @@
   shellHook,
 }:
 let
+  vfio-user-socket = "/tmp/usbvfiod-vfio-user.sock";
+  hotplug-socket = "/tmp/usbvfiod-hotplug.sock";
+
   sshhost = pkgs.writeShellScriptBin "sshhost" ''
     ssh -p 2000 root@localhost -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
   '';
 
   sshguest = pkgs.writeShellScriptBin "sshguest" ''
     ssh -p 2000 root@localhost -o ProxyCommand="ssh -W %h:%p -p 2000 root@localhost -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+  '';
+
+  runusbvfiod = pkgs.writeShellScriptBin "runusbvfiod" ''
+    cargo build --bin usbvfiod
+    rm ${vfio-user-socket}
+    rm ${hotplug-socket}
+    ./target/debug/usbvfiod --socket-path ${vfio-user-socket} --hotplug-socket-path ${hotplug-socket} --no-color
+  '';
+
+  runchv = pkgs.writeShellScriptBin "runchv" ''
+    nix-build -A netboot.x86_64-linux '<nixpkgs/nixos/release.nix>'
+    NETBOOT=$(grep -o "init=[^$]*" result/netboot.ipxe)
+    echo "$NETBOOT"
+    cloud-hypervisor --memory size=4G,shared=on --serial tty --user-device socket=${vfio-user-socket} --console off --kernel result/bzImage --initramfs result/initrd --cmdline "$NETBOOT console=ttyS0"
+  '';
+
+  attach = pkgs.writeShellScriptBin "attach" ''
+    cargo build --bin remote
+    ./target/debug/remote --socket ${hotplug-socket} --attach /dev/bus/usb/$1/$2
   '';
 in
 {
@@ -18,6 +40,9 @@ in
     packages = [
       sshhost
       sshguest
+      runusbvfiod
+      runchv
+      attach
     ];
   };
 }
