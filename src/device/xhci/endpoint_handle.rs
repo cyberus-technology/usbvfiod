c1c1596e532ef5037901d50d96f9bb2dff39b20f
Noi0103 <noi0103@proton.me>
2026-09-11 14:15:16 +0200

    endpoint_handle: add a test to submit an illegal sequence
    Xhci specification chapter 3.2.9 Control Transfers:
    
    > A Data Stage TD consists of a Data Stage TRB followed by zero or more
    Normal TRBs.

--- src/device/xhci/endpoint_handle.rs
+++ src/device/xhci/endpoint_handle.rs
@@ -1421,6 +1421,7 @@ pub mod tests {
     const FIRST_ADDRESS: u64 = 0x10;
     const SECOND_ADDRESS: u64 = 0x20;
     const THIRD_ADDRESS: u64 = 0x30;
+    const FOURTH_ADDRESS: u64 = 0x40;
 
     const DMA_POINTER_1: u64 = 0x200;
 
@@ -1720,6 +1721,91 @@ pub mod tests {
         assert_eq!(
             interrupter.await_event().await,
             Some(expected_event(THIRD_ADDRESS, 0, false))
+        );
+
+        assert!(interrupter.is_empty());
+    }
+
+    #[tokio::test]
+    async fn submit_second_illegal_data_stage_trb() {
+        let (mut interrupter, mut control_endpoint) =
+            init_control_endpoint_handle_test(MockRealControlEndpointReadStatic::new());
+
+        let setup_stage = RawTrbBuilder::new(FIRST_ADDRESS)
+            .with_setup_type(SETUP_BM_REQUEST_TYPE_IN)
+            .with_setup_wlength(SETUP_WLENGTH)
+            .with_immediate_data()
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_SETUP_STAGE)
+            .with_byte(14, SETUP_TRANSFER_TYPE_IN_DATA)
+            .build();
+        let data_stage_1 = RawTrbBuilder::new(SECOND_ADDRESS)
+            .with_data_pointer(DMA_POINTER_1)
+            .with_trb_transfer_length(TRANSFER_LENGTH)
+            .with_chain()
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_DATA_STAGE)
+            .with_direction()
+            .build();
+        let data_stage_2 = RawTrbBuilder::new(THIRD_ADDRESS)
+            .with_data_pointer(DMA_POINTER_1)
+            .with_trb_transfer_length(TRANSFER_LENGTH)
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_DATA_STAGE)
+            .with_direction()
+            .build();
+        let status_stage = RawTrbBuilder::new(FOURTH_ADDRESS)
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_STATUS_STAGE)
+            .with_direction()
+            .build();
+
+        let input_trb = vec![setup_stage, data_stage_1, data_stage_2, status_stage];
+
+        for trb in input_trb.clone() {
+            control_endpoint
+                .submit_trb(trb)
+                .expect("this mock hardware request should never fail");
+            control_endpoint.next_completion().await.ok();
+        }
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(FIRST_ADDRESS, 0, false))
+        );
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(SECOND_ADDRESS, 0, false))
+        );
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(EventTrb::new_transfer_event_trb(
+                THIRD_ADDRESS,
+                0,
+                CompletionCode::TrbError,
+                false,
+                ENDPOINT_ID,
+                SLOT_ID,
+            ))
+        );
+
+        // Usually an endpoint should halt after encountering an error.
+
+        // This test does not include a full endpoint and thus can not stop.
+
+        // If we do not halt we expect to encounter the rest of the Control Request
+        // to be unexpected/out of order TRB's and send a CompletionCode::TrbError.
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(EventTrb::new_transfer_event_trb(
+                FOURTH_ADDRESS,
+                0,
+                CompletionCode::TrbError,
+                false,
+                ENDPOINT_ID,
+                SLOT_ID,
+            ))
         );
 
         assert!(interrupter.is_empty());
