c634c08fe39d7f8cd4a37e36b32afa522bf86c19
Noi0103 <noi0103@proton.me>
2026-09-17 16:22:14 +0200

    endpoint_handle: add a test with interleaved event data
    Xhci specification chapter 4.11.5.2 Event Data TRB:
    
    > - The Length field of an Event Data Transfer Event shall reflect the
    number of bytes transferred from the beginning of a TD or since the last
    Event TRB encountered in a TD.

--- src/device/xhci/endpoint_handle.rs
+++ src/device/xhci/endpoint_handle.rs
@@ -2435,6 +2435,93 @@ pub mod tests {
     }
 
     #[tokio::test]
+    async fn submit_control_in_request_with_interleaved_event_data_in_the_data_td() {
+        let (mut interrupter, mut control_endpoint) =
+            init_control_endpoint_handle_test(MockRealControlEndpointReadStatic::new(None));
+
+        let setup_stage = RawTrbBuilder::new(FIRST_ADDRESS)
+            .with_setup_type(SETUP_BM_REQUEST_TYPE_IN)
+            .with_setup_wlength(SETUP_WLENGTH * 2)
+            .with_immediate_data()
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_SETUP_STAGE)
+            .with_byte(14, SETUP_TRANSFER_TYPE_IN_DATA)
+            .build();
+        let data_stage = RawTrbBuilder::new(SECOND_ADDRESS)
+            .with_data_pointer(DMA_POINTER_1)
+            .with_trb_transfer_length(TRANSFER_LENGTH)
+            .with_chain()
+            .with_trb_type(TRB_TYPE_DATA_STAGE)
+            .with_direction()
+            .build();
+        let event_data_1 = RawTrbBuilder::new(THIRD_ADDRESS)
+            .with_data_pointer(EVENT_DATA_FIELD + 1)
+            .with_chain()
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_EVENT_DATA)
+            .with_direction()
+            .build();
+        let normal = RawTrbBuilder::new(FOURTH_ADDRESS)
+            .with_data_pointer(DMA_POINTER_2)
+            .with_trb_transfer_length(TRANSFER_LENGTH)
+            .with_chain()
+            .with_trb_type(TRB_TYPE_NORMAL)
+            .build();
+        let event_data_2 = RawTrbBuilder::new(FIFTH_ADDRESS)
+            .with_data_pointer(EVENT_DATA_FIELD + 2)
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_EVENT_DATA)
+            .with_direction()
+            .build();
+        let status_stage = RawTrbBuilder::new(SIXTH_ADDRESS)
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_STATUS_STAGE)
+            .with_direction()
+            .build();
+
+        let input_trb = vec![
+            setup_stage,
+            data_stage,
+            event_data_1,
+            normal,
+            event_data_2,
+            status_stage,
+        ];
+
+        for trb in input_trb {
+            control_endpoint
+                .submit_trb(trb)
+                .expect("this mock hardware request should never fail");
+            assert_eq!(
+                control_endpoint
+                    .next_completion()
+                    .await
+                    .expect("this mock hardware request should never fail"),
+                TrbProcessingResult::Ok
+            );
+        }
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(FIRST_ADDRESS, 0, false))
+        );
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(EVENT_DATA_FIELD + 1, 512, true))
+        );
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(EVENT_DATA_FIELD + 2, 512, true))
+        );
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(SIXTH_ADDRESS, 0, false))
+        );
+
+        assert!(interrupter.is_empty());
+    }
+
+    #[tokio::test]
     async fn submitting_out_of_order_or_unfinished_sequence_does_not_prevent_the_following_valid_sequence_of_trb(
     ) {
         let (mut interrupter, mut control_endpoint) =
