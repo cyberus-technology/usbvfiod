89b55d837a63cdb0bbd97cb68fe9bf89c2e09871
Noi0103 <noi0103@proton.me>
2026-09-23 13:35:54 +0200

    endpoint_handle: add a test that follows a short packet event with event data trb
    This is a bit tricky to decide with the spec.
    1. The edtla should reset after event data read it to report transferred
    bytes.
    2. The event data after short packet shall include the number of
    transferred bytes of the TD.
    3. With PAE (Parse all Event Data) we need to react to more than one
    event data after the short packet event.
    
    Conclusion: We reset the edtla as ordered, the second and following
    event data will read a edtla of 0 with `CompletionCode::ShortPacket`.
    
    Xhci specification chapter 4.11.5.2 Event Data TRB:
    
    > - When an Event Data TRB is encountered an Event Data Transfer Event
    shall be generated, where the TRB Transfer Length field shall contain
    the value of the EDTLA. The EDTLA shall then be cleared to ‘0’ and begin
    accumulating again.
    
    Xhci specification chapter 4.10.1.1.1 Short Transfers when using Event
    Data TRBs:
    
    > - Automatically advance the Dequeue Pointer for the Transfer Ring to
    the beginning of the next TD.
    >   - When an Event Data TRB is encountered in the process of advancing
    the Dequeue Pointer from the Short Packet TRB to the beginning of the
    next TD, the xHC shall parse the Event Data TRB, i.e. if the IOC flag is
    set in the Event Data TRB, an Event Data Transfer Event shall be
    generated with the Completion Code set to Short Packet and the Length
    field set to the actual number of bytes received by the TD
    
    > If subsequent Event Data TRBs are encountered in the process of
    advancing the Dequeue Pointer from the first Event Data TRB encountered
    to the 192 beginning of the next TD, the xHC shall parse them if the
    Parse All Event Data (PAE) flag is set (‘1’), and shall not parse them
    if the PAE flag is cleared (‘0’). Refer to section 5.3.6 for more
    information on PAE.

--- src/device/xhci/endpoint_handle.rs
+++ src/device/xhci/endpoint_handle.rs
@@ -3229,4 +3229,109 @@ pub mod tests {
 
         assert!(interrupter.is_empty());
     }
+
+    #[tokio::test]
+    async fn long_control_request_returns_short_packet_and_has_subsequent_trb_including_event_data()
+    {
+        const SHORT_AFTER_BYTES: u32 = 300;
+        let (mut interrupter, mut control_endpoint) = init_control_endpoint_handle_test(
+            MockRealControlEndpointReadStatic::new(Some(SHORT_AFTER_BYTES)),
+        );
+
+        let setup_stage = RawTrbBuilder::new(FIRST_ADDRESS)
+            .with_setup_type(SETUP_BM_REQUEST_TYPE_IN)
+            .with_setup_wlength(SETUP_WLENGTH)
+            .with_immediate_data()
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_SETUP_STAGE)
+            .with_byte(14, SETUP_TRANSFER_TYPE_IN_DATA)
+            .build();
+        let data_stage = RawTrbBuilder::new(SECOND_ADDRESS)
+            .with_data_pointer(DMA_POINTER_1)
+            .with_trb_transfer_length(TRANSFER_LENGTH)
+            .with_chain()
+            .with_trb_type(TRB_TYPE_DATA_STAGE)
+            .with_direction()
+            .build();
+        let event_data_1 = RawTrbBuilder::new(THIRD_ADDRESS)
+            .with_data_pointer(EVENT_DATA_FIELD + 1)
+            .with_chain()
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_EVENT_DATA)
+            .with_direction()
+            .build();
+        let normal = RawTrbBuilder::new(FOURTH_ADDRESS)
+            .with_data_pointer(DMA_POINTER_2)
+            .with_trb_transfer_length(TRANSFER_LENGTH)
+            .with_chain()
+            .with_trb_type(TRB_TYPE_NORMAL)
+            .with_direction()
+            .build();
+        let event_data_2 = RawTrbBuilder::new(FIFTH_ADDRESS)
+            .with_data_pointer(EVENT_DATA_FIELD + 2)
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_EVENT_DATA)
+            .with_direction()
+            .build();
+        let status_stage = RawTrbBuilder::new(SIXTH_ADDRESS)
+            .with_interrupt_on_completion()
+            .with_trb_type(TRB_TYPE_STATUS_STAGE)
+            .with_direction()
+            .build();
+
+        let input_trb = vec![
+            setup_stage,
+            data_stage, // triggers short packet
+            event_data_1,
+            normal, // subsequent after a short packet
+            event_data_2,
+            status_stage,
+        ];
+
+        for trb in input_trb.clone() {
+            control_endpoint
+                .submit_trb(trb)
+                .expect("this mock hardware request should never fail");
+            assert_eq!(
+                control_endpoint.next_completion().await.ok(),
+                Some(TrbProcessingResult::Ok)
+            );
+        }
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(FIRST_ADDRESS, 0, false))
+        );
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(EventTrb::new_transfer_event_trb(
+                EVENT_DATA_FIELD + 1,
+                SHORT_AFTER_BYTES,
+                CompletionCode::ShortPacket,
+                true,
+                ENDPOINT_ID,
+                SLOT_ID,
+            ))
+        );
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(EventTrb::new_transfer_event_trb(
+                EVENT_DATA_FIELD + 2,
+                0,
+                CompletionCode::ShortPacket,
+                true,
+                ENDPOINT_ID,
+                SLOT_ID,
+            ))
+        );
+
+        assert_eq!(
+            interrupter.await_event().await,
+            Some(expected_event(SIXTH_ADDRESS, 0, false))
+        );
+
+        assert!(interrupter.is_empty());
+    }
 }
